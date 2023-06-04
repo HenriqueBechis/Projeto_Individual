f785cdfbf9d75f5ce181cff1d71a0bb2aceeb30b
HenriqueBechis <Henrique.coelho@sptech.school>
2023-06-04 16:27:31 -0300

    Organização das pastas e inicio do feed

---
+++ site/src/database/Musica.sql
@@ -0,0 +1,59 @@
+-- Arquivo de apoio, caso você queira criar tabelas como as aqui criadas para a API funcionar.
+-- Você precisa executar os comandos no banco de dados para criar as tabelas,
+-- ter este arquivo aqui não significa que a tabela em seu BD estará como abaixo!
+
+/*
+comandos para mysql - banco local - ambiente de desenvolvimento
+*/
+
+CREATE DATABASE musica;
+
+USE musica;
+
+CREATE TABLE usuario (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	nome VARCHAR(50),
+	email VARCHAR(50),
+	senha VARCHAR(50),
+	CPF CHAR(11)
+);
+SELECT * FROM usuario;
+
+DESC USUARIO;
+CREATE TABLE aviso (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	titulo VARCHAR(100),
+	descricao VARCHAR(150),
+	fk_usuario INT,
+	FOREIGN KEY (fk_usuario) REFERENCES usuario(id)
+);
+SELECT * FROM aviso;
+
+/*
+comando para sql server - banco remoto - ambiente de produção
+*/
+
+CREATE TABLE usuario (
+	id INT PRIMARY KEY IDENTITY(1,1),
+	nome VARCHAR(50),
+	email VARCHAR(50),
+	senha VARCHAR(50),
+);
+
+CREATE TABLE aviso (
+	id INT PRIMARY KEY IDENTITY(1,1),
+	titulo VARCHAR(100),
+	descricao VARCHAR(150),
+	fk_usuario INT FOREIGN KEY REFERENCES usuario(id)
+);
+
+create table aquario (
+/* em nossa regra de negócio, um aquario tem apenas um sensor */
+	id INT PRIMARY KEY IDENTITY(1,1),
+	descricao VARCHAR(300)
+);
+
+/* esta tabela deve estar de acordo com o que está em INSERT de sua API do arduino - dat-acqu-ino */
+
+
+
